70e7455eeab1e481be57d1e3bcaa30ba386a8dfb
Neil Penades <neilpenades65@gmail.com>
2025-11-20 15:39:40 +0100

    Comit desplegament github inicial tres

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://galan170.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docosaurusNeilPG/',
+  baseUrl: '/docosaurusNeilPG',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
